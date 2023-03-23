5f6d87d775aef8b0e73a172376ac114808021825
James Addison <siliconjungle.io@gmail.com>
2023-03-23 11:03:19 +1100

    signatures

--- src/tiny-merge/signatures.js
+++ src/tiny-merge/signatures.js
@@ -14,11 +14,44 @@ export const generateKeyPair = () => new Promise((resolve, reject) => {
   })
 })
 
+export const jsonToString = (json) => {
+  if (json === undefined || json === null) {
+    return ''
+  }
+
+  if (Array.isArray(json)) {
+    return json.map(jsonToString).join(',')
+  }
+
+  if (typeof json === 'object') {
+    return Object.keys(json)
+      .sort()
+      .map((key) => `${key}:${jsonToString(json[key])}`)
+      .join(',')
+  }
+
+  if (typeof json === 'string') {
+    return json
+  }
+
+  if (typeof json === 'number') {
+    return json.toString()
+  }
+
+  if (typeof json === 'boolean') {
+    return json.toString()
+  }
+
+  // unknown types should be ignored?
+  // or maybe an error should be thrown
+  return ''
+}
+
 export const signData = async (privateKey, data) =>
-  crypto.sign('SHA256', JSON.stringify(data), privateKey)
+  crypto.sign('SHA256', jsonToString(data), privateKey)
 
 export const verifySignature = async (publicKey, signature, data) =>
-  crypto.verify('SHA256', JSON.stringify(data), publicKey, signature)
+  crypto.verify('SHA256', jsonToString(data), publicKey, signature)
 
 export const toString = (buffer) => buffer.toString('hex')
 export const toBuffer = (string) => Buffer.from(string, 'hex')
